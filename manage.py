455e8f3ac16ad698bf94f8d1735d0488ce7b1601
Moreshwar Mahale <91116410+codewithmoru@users.noreply.github.com>
2022-01-28 16:09:14 +0530

    Update manage.py
    new

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lawdiary.herokuapp.com']
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
