76c7205d9545afe293fb5bb159880fae110e4181
Moreshwar Mahale <91116410+codewithmoru@users.noreply.github.com>
2022-02-13 22:08:13 -0800

    Update settings.py
    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lawwrite.herokuapp.com']
+ALLOWED_HOSTS = ['lawnoteapp.herokuapp.com']
 
 
 # Application definition
